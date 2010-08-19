06fdf7628203843166de5ff99d76933e5e8c444c
Christopher Burnett <signalstatic@gmail.com>
2010-08-18 23:16:31 -0700

    whitespace

--- lib/server/server.rb
+++ lib/server/server.rb
@@ -3,30 +3,29 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../..', 'lib'))
 
-require 'io_tools'
-dir = File.expand_path(File.dirname(__FILE__))
+@dir = File.expand_path(File.dirname(__FILE__))
 
-require dir+'/importers/posterous_feed_importer'
-require dir+'/importers/posterous_api_importer'
-require dir+'/importers/word_press_importer'
+require 'io_tools'
+
+require dir + '/importers/posterous_feed_importer'
+require dir + '/importers/posterous_api_importer'
+require dir + '/importers/word_press_importer'
 
 module IoTools
-  
-  
   class Server < Sinatra::Base
-    dir = File.dirname(File.expand_path(__FILE__))
 
-    set :views,  "#{dir}/views"
-    set :public, "#{dir}/public"
+    set :views,  "#{@dir}/views"
+    set :public, "#{@dir}/public"
     
     get "/" do
       haml :index
     end
 
     post '/import' do
-      klass = params.delete('service').camelize.constantize
-      @import = klass.new(params)
-      @posts = @import.import!
+      @klass    = params.delete('service').camelize.constantize
+      @import   = klass.new(params)
+      @posts    = @import.import!
+
       haml :import
     end
 
